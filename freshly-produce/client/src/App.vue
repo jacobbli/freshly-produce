32e3c40055c268b875e8dcaba69001fefb84a2a4
SmallYellowFace <jctheathlete@gmail.com>
2021-02-06 15:30:52 -0800

    added menubar

--- freshly-produce/client/src/App.vue
+++ freshly-produce/client/src/App.vue
@@ -1,16 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NavBar/>
+    <Card>
+      <template #header>
+          <img alt="user header" src="demo/images/usercard.png">
+      </template>
+      <template #title>
+          Advanced Card
+      </template>
+      <template #content>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt
+          quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+      </template>
+      <template #footer>
+          <Button icon="pi pi-check" label="Save" />
+          <Button icon="pi pi-times" label="Cancel" class="p-button-secondary" style="margin-left: .5em" />
+      </template>
+  </Card>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    NavBar,
+  },
 }
 </script>
 
@@ -21,6 +38,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+body {
+   margin: 0px;
+   background-color: #F8F9FA;
 }
 </style>
--- freshly-produce/client/src/main.js
+++ freshly-produce/client/src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './App.vue';
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/saga-blue/theme.css'       //theme
+import 'primevue/resources/primevue.min.css'                 //core css
 
-createApp(App).mount('#app')
+import Dialog from 'primevue/dialog';
+import Menubar from 'primevue/menubar';
+import Card from 'primevue/card';
+
+const app = createApp(App);
+app.use(PrimeVue);
+// createApp(App).mount('#app')
+
+app.component('Dialog', Dialog);
+app.component('Card', Card);
+app.component('Menubar', Menubar);
+
+app.mount('#app');
