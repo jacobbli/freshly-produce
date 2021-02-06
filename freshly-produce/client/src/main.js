6570c1b07403abc8c133c0585d57ef5522cf7d38
Jacob Lee <jlee93115@163.com>
2021-02-06 10:58:28 -0800

    Initial client setup

--- freshly-produce/client/src/main.js
+++ freshly-produce/client/src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import PrimeVue from 'primevue/config';
+import InputText from 'primevue/inputtext';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router);
+app.use(PrimeVue);
+app.component('InputText', InputText);
+
+app.mount('#app')
