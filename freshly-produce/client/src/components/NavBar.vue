2ece56270407c617d95ecc7fe95e5b7eb366f036
Jacob Lee <jlee93115@163.com>
2021-02-07 14:17:09 -0800

    Add routes to navbar

--- freshly-produce/client/src/components/NavBar.vue
+++ freshly-produce/client/src/components/NavBar.vue
@@ -1,47 +1,53 @@
 <template >
     <div id="navbar">
-        <Sidebar class="p-sidebar-sm" v-model:visible="visibleLeft">
-            <img alt="logo" src="/images/freshlyproduce.png" height="30" class="p-mx-4">
-            <Menu class="p-pt-4 p-m-0 p-shadow-0" :model="menuitems" />
-        </Sidebar>
-        <Menubar :model="items" >
+        <Menubar>
             <template #start>
                 <Button  icon="pi pi-bars" @click="visibleLeft = true" />
                 <img alt="logo" src="/images/freshlyproduce.png" height="40" class="p-mx-4">
             </template>
         </Menubar>
+          <Sidebar class="p-sidebar-sm" v-model:visible="visibleLeft">
+            <img alt="logo" src="/images/freshlyproduce.png" height="30" class="p-mx-4">
+            <Menu class="p-pt-4 p-m-0 p-shadow-0" :model="menuitems" />
+        </Sidebar>
     </div>
 </template>
 <script>
 
 export default {
 
-    data() {
-        return {
-            visibleLeft : false,
-            menuitems: [
-                {
-                    items:[
-                        {
-                            label:'Close-To-Expiry',
-                            icon:'pi pi-clock',
-                            to: ''
-                        },
-                        {
-                            label:'Subscription',
-                            icon:'pi pi-bell',
-                            to: ''
-                        },                        
-                        {
-                            label:'My Subscription',
-                            icon:'pi pi-tags',
-                            to: ''
-                        },
-                    ]
-                }
-            ]
+  data() {
+    return {
+      visibleLeft : false,
+      menuitems: [
+        {
+          label:'Close-To-Expiry',
+          icon:'pi pi-clock',
+          to: ''
+        },
+        {
+          label:'Available Subscriptions',
+          icon:'pi pi-bell',
+          to: '/available-subscriptions'
+        },
+        {
+          label:'My Subscriptions',
+          icon:'pi pi-tags',
+          to: '/my-subscriptions'
+        },
+        {
+          label:'Create Subscription',
+          icon:'pi pi-tags',
+          to: '/subscriptions/create'
+        },
+        {
+          label:'My Offered Subscriptions',
+          icon:'pi pi-tags',
+          to: '/my-offers'
         }
+      ]
     }
+  }
 }
 </script>
 <style >
