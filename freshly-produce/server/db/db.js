13bb848ebe2edca8e9bf238b6069f5c0cf35435f
Jacob Lee <jlee93115@163.com>
2021-02-08 12:23:42 -0800

    Use env variables for pg authentication

--- freshly-produce/server/db/db.js
+++ freshly-produce/server/db/db.js
@@ -1,14 +1,14 @@
 //connect to postgres
 const Pool = require('pg').Pool;
 
-const pool = new Pool({  
-        host: 'localhost',  
-        user: 'postgres',  
-        database: 'postgres',  
-        password: 'postgres',
-        port: 5432,
+const pool = new Pool({
+        host: process.env.PGHOST,
+        user: process.env.PGDATABASE,
+        database: process.env.PGHOST,
+        password: process.env.PGPASSWORD,
+        port: process.env.PGPORT,
         ssl: false
-    });  
+    });
 
 
 module.exports = pool;
