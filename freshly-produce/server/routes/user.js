1def67fd42b3043c02fbca53c4592663426c661f
Jacob Lee <jlee93115@163.com>
2021-02-08 23:40:44 -0800

    Implement api to create and view products belonging to a user

--- freshly-produce/server/routes/user.js
+++ freshly-produce/server/routes/user.js
@@ -6,10 +6,15 @@ const userController = require('../controller/user-controller');
 /* GET users */
 router.post('/login', userController.getUser);
 
-// GET all products owned by a user
-router.get('/products', userController.getProducts);
 
 // POST new user
 router.post('/', userController.addUser);
+
+// GET all products owned by a user
+router.get('/products', userController.getProducts);
+
+// POST new product
+router.post('/products', userController.addProduct);
+
 
 module.exports = router;
