c0fe2784fd712c41c2bacf0b963b3fec30f371d2
Jacob Lee <jlee93115@163.com>
2021-02-10 00:10:43 -0800

    Implement delete product

--- freshly-produce/client/src/api/ProductsApi.js
+++ freshly-produce/client/src/api/ProductsApi.js
@@ -11,3 +11,15 @@ export async function getProducts(reqObject) {
     return Promise.reject('Request failed');
   }
 }
+
+export async function deleteProduct(reqObject) {
+  try {
+    let endpointUrl = `${process.env.VUE_APP_ROOT_URL}/product`;
+    let response = await axios.delete(endpointUrl, {
+      params: reqObject
+    });
+    return Promise.resolve(response.data);
+  } catch(err) {
+    return Promise.reject('Request failed');
+  }
+}
