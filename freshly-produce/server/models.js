947bdc89241be48124933bbb411a42492a9d69fd
Jacob Lee <jlee93115@163.com>
2021-02-10 00:19:17 -0800

    Create models for frequently used objects

---
+++ freshly-produce/server/models.js
@@ -0,0 +1,17 @@
+export const PRODUCT_TYPE = Object.freeze({
+  "subscription":1, 
+  "discounted_produce":2
+})
+
+export const USER_ROLE = Object.freeze({
+  "farmer":1, 
+  "customer":2
+})
+
+export const WEEK_DAY = Object.freeze({
+  "Monday":1, 
+  "Tuesday":2,
+  "Wednesday":3,
+  "Thursday":4,
+  "Friday":5
+})
