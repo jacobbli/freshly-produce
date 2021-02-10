8de97b3ac949a1af3d70a10a79621e58fab1ed7b
Jacob Lee <jlee93115@163.com>
2021-02-10 00:47:17 -0800

    Modify addProduct api

--- freshly-produce/client/src/api/ProductsApi.js
+++ freshly-produce/client/src/api/ProductsApi.js
@@ -4,8 +4,7 @@
 export default {
     addProduct: function(data) {
         let endpointUrl = `${process.env.VUE_APP_ROOT_URL}/product/add`;
-        console.log(endpointUrl)
-        axios.post("http://localhost:3000/product/add",data).then(()=>{
+        axios.post(endpointUrl,data).then(()=>{
             this.$toast.add({severity:'success', summary: 'Submited!', life: 3000,});
             this.$router.push({ name: 'HomePage' });
         });
