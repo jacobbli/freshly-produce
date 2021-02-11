92d11e32f0c382744395fb038f1b32c7b88a7b3e
Jacob Lee <jlee93115@163.com>
2021-02-10 23:05:57 -0800

    Modify interface of dataview table

--- freshly-produce/client/src/views/FarmerDiscountedProducePage.vue
+++ freshly-produce/client/src/views/FarmerDiscountedProducePage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="layout-content">
     <h1>My Discounted Produce</h1>
     <DataView :value="listProduct" :layout="layout" :paginator="true" :rows="9" :sortOrder="sortOrder" :sortField="sortField">
       <template #header>
@@ -14,47 +14,38 @@
       </template>
 
       <template #grid="slotProps">
-          <div class="p-col-3 p-p-4">
-              <Card>
-                  <template #header>
-                      <img alt="user header" src="/images/temp/broccoli.jpg">
-                  </template>
-                  <template #title>
-                      {{slotProps.data.product_name}}
-                  </template>
-                  <template  #content>
-                      <i class="pi pi-tag product-category-icon p-pr-2"></i><span class="product-category">{{slotProps.data.product_type}}</span>
-                      <div class="product-price p-pr-4">${{slotProps.data.product_price}}</div>
-                  </template>
-                  <template #footer>
-                    <div class="p-grid p-col-12 p-m-0">
-                      <div class="p-col-6 p-m-0">
-                        <Button
-                          v-if="slotProps.data.is_published"
-                          icon="pi pi-times"
-                          class="p-button-warning "
-                          label="Unpublish"
-                          @click="openConfirmationModal(slotProps.data, 'unpublish')" />
-                        <Button
-                          v-else
-                          icon="pi pi-check"
-                          class="p-col-6 p-m-0"
-                          label="Publish"
-                          @click="openConfirmationModal(slotProps.data, 'publish')" />
-                      
-                      </div>
-                      <div class="p-col-6 p-m-0">
-                        <Button
-                          icon="pi pi-times"
-                          class="p-button-danger p-col-6 p-m-0"
-                          label="Delete"
-                          @click="openConfirmationModal(slotProps.data, 'delete')" />
-                      
-                      </div>
-                    </div>
-                  </template>
-              </Card>
-          </div>
+          <div class="p-col-12 p-md-4">
+            <div class="product-grid-item card">
+                <div class="product-grid-item-top">
+                    <span class="product-category">
+                      <Button class="p-button-sm edit-button" icon="pi pi-pencil" />
+                      <Button
+                        class="p-button-sm p-button-danger"
+                        icon="pi pi-times"
+                        @click="openConfirmationModal(slotProps.data, 'delete')"/>
+                    </span>
+                </div>
+                <div class="product-grid-item-content">
+                    <img alt="user header" :src="slotProps.data.product_photo" />
+                    <div class="product-name">{{slotProps.data.product_name}}</div>
+                    <div class="product-description">{{slotProps.data.product_description}}</div>
+                </div>
+                <div class="product-grid-item-bottom">
+                    <span class="product-price">${{slotProps.data.product_price}}</span>
+                    <Button
+                    v-if="slotProps.data.is_published"
+                    icon="pi pi-times"
+                    class="p-button-warning"
+                    label="Unpublish"
+                    @click="openConfirmationModal(slotProps.data, 'unpublish')" />
+                  <Button
+                    v-else
+                    icon="pi pi-check"
+                    label="Publish"
+                    @click="openConfirmationModal(slotProps.data, 'publish')" />
+                </div>
+            </div>
+        </div>
       </template>
     </DataView>
 
@@ -208,7 +199,80 @@ export default {
 </script>
 
 <style scoped>
-.p-card-footer button {
+.p-dropdown {
+    width: 14rem;
+    font-weight: normal;
+}
+
+.product-name {
+	font-size: 1.5rem;
+	font-weight: 700;
+}
+
+.product-description {
+	margin: 0 0 1rem 0;
+}
+
+.edit-button {
+  margin-right: .5rem;
+}
+
+.product-category-icon {
+	vertical-align: middle;
 	margin-right: .5rem;
+}
+
+.product-category {
+	font-weight: 600;
+	vertical-align: middle;
+}
+
+.product-grid-item {
+	margin: .5rem;
+	border: 1px solid #dee2e6;
+}
+.product-grid-item-top {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  margin: .5rem;
+}
+
+.product-grid-item-bottom {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: .5rem;
+}
+
+img {
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  margin: 2rem 0;
+}
+
+.product-grid-item-content {
+  text-align: center;
+}
+
+.product-grid-item-content img {
+  width: 75%
+
+}
+
+.product-price {
+  font-size: 1.5rem;
+  font-weight: 600;
+}
+
+.no-subscriptions-button {
+  margin-top: 300px;
+}
+
+.layout-content .card {
+  background: #ffffff;
+  padding: 2rem;
+  box-shadow: 0 2px 1px -1px rgba(0,0,0,.2), 0 1px 1px 0 rgba(0,0,0,.14), 0 1px 3px 0 rgba(0,0,0,.12);
+  border-radius: 4px;
+  margin-bottom: 2rem;
 }
 </style>
