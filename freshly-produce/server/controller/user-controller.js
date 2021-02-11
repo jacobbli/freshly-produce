863357db200e2209e8c3bc2f7a1fc632aa00a0d7
Jacob Lee <jlee93115@163.com>
2021-02-10 19:32:32 -0800

    Rearranged subscription related api

--- freshly-produce/server/controller/user-controller.js
+++ freshly-produce/server/controller/user-controller.js
@@ -1,6 +1,5 @@
 let userModel = require('../model/user-model');
 const bcrypt = require('bcrypt');
-const formidable = require('formidable');
 
 async function getUser(request, response) {
   try {
@@ -10,6 +9,7 @@ async function getUser(request, response) {
     }
     const match = await bcrypt.compare(request.body.password, res[0].password);
     if (match) {
+      delete res[0].password;
       return Promise.resolve(response.json(res[0]));
     } else {
       return Promise.reject(response.status(403).end());
@@ -47,143 +47,7 @@ async function addUser(request, response) {
   }
 }
 
-
-async function getPurchasedProducts(request, response) {
-  try {
-    const orderArgs = [request.query.user_id, request.query.product_type, false];
-    const orders = await userModel.getOrders(orderArgs);
-
-    if (orders.length > 0) {
-      const productArgs = [request.query.user_id, request.query.product_type];
-      const products = await userModel.getProducts(productArgs);
-
-      if(products.length > 0) {
-        response.json(products);
-      }
-    }
-    response.json([]);
-  } catch(error) {
-    console.error(error)
-    response.status(404).end();
-  }
-}
-
-async function getOfferedProducts(request, response) {
-  try {
-    if (request.query.user_id && request.query.product_type) {
-      const productArgs = [request.query.user_id, request.query.product_type];
-      const products = await userModel.getProducts(productArgs);
-      response.json(products);
-    } else {
-      response.status(404).end();
-    }
-  } catch(error) {
-    console.error(error)
-    response.status(404).end();
-  }
-}
-
-function addSubscription(request, response) {
-  const form = formidable();
-  form.parse(request, (err, fields, files) => {
-    if (err) {
-      next(err);
-      response.status(404).end();
-    }
-    const created_at = new Date();
-    const arg = [
-      fields.user_id,
-      files.product_photo,
-      fields.product_name,
-      fields.product_type,
-      fields.product_price,
-      fields.product_description,
-      fields.unit,
-      fields.quantity,
-      fields.frequency,
-      fields.delivery_day,
-      false,
-      false,
-      created_at
-    ]
-    userModel.insertProduct(arg).then(res => {
-      response.json(res);
-    }).catch(error => {
-      console.error(error)
-      response.status(404).end();
-    });
-  });
-}
-
-function publishProduct(request, response) {
-  const args = [request.body.product_id];
-  userModel.publishProduct(args).then(res => {
-    response.json(res);
-  }).catch(error => {
-    console.error(error)
-    response.status(404).end();
-  });
-}
-
-function unpublishProduct(request, response) {
-  const args = [request.body.product_id];
-  userModel.unpublishProduct(args).then(res => {
-    response.json(res);
-  }).catch(error => {
-    console.error(error)
-    response.status(404).end();
-  });
-}
-
-function subscribe(request, response) {
-  const created_at = new Date();
-  const arg = [
-    request.body.transaction_cost,
-    created_at,
-    request.body.user_id,
-    false
-  ]
-  // TODO: create sql trigger to update transaction table?
-  userModel.insertTransaction(arg).then(res => {
-    orderArg = [
-      request.body.product_price,
-      request.body.product_quantity,
-      created_at,
-      request.body.user_id,
-      res.transaction_id,
-      request.body.product_id,
-      request.body.product_type,
-      false
-    ];
-    userModel.insertOrder(orderArg).then(() => {
-      response.json(res);
-    })
-  }).catch(error => {
-    console.error(error)
-    response.status(404).end();
-  });
-}
-
-function unsubscribe(request, response) {
-  const arg = [
-    request.body.user_id,
-    request.body.product_id
-  ]
-  userModel.unsubscribe(arg).then(res => {
-    response.json(res);
-  }).catch(error => {
-    console.error(error)
-    response.status(404).end();
-  });
-}
-
 module.exports = {
   getUser,
-  getPurchasedProducts,
-  getOfferedProducts,
-  addUser,
-  addSubscription,
-  publishProduct,
-  unpublishProduct,
-  unsubscribe
+  addUser
 }
--- freshly-produce/server/model/user-model.js
+++ freshly-produce/server/model/user-model.js
@@ -2,47 +2,12 @@ let db = require('../db/db');
 
 async function getUser(user_id) {
   try {
-    const query = `SELECT * FROM users WHERE users.username = $1;`;
+    const query = `SELECT user_id, password, username, first_name, surname, role, phone, email, address FROM users WHERE users.username = $1;`;
     const args = [user_id];
 
     const res = await db.query(query, args);
     return Promise.resolve(res.rows);
   } catch (error) {
-    return Promise.resolve(error);
-  }
-}
-
-async function getOrders(orderArgs) {
-  try {
-    const query = `SELECT * FROM orders WHERE buyer_id = $1 AND order_type = $2 AND is_deleted = $3;`;
-    const res = await db.query(query, orderArgs);
-    return Promise.resolve(res.rows);
-  } catch (error) {
-    console.error(error)
-    return Promise.resolve(error);
-  }
-}
-
-async function getProducts(productArgs) {
-  try {
-    const query = `SELECT * FROM products WHERE seller_id = $1 AND product_type = $2 AND is_deleted = false;;`;
-    const res = await db.query(query, productArgs);
-    return Promise.resolve(res.rows);
-  } catch (error) {
-    console.error(error)
-    return Promise.resolve(error);
-  }
-}
-
-async function insertProduct(args){
-  try {
-    const query = `INSERT INTO products (seller_id, product_photo, product_name, product_type, product_price, product_description, unit, quantity, frequency, delivery_day, is_published, is_deleted, created_at)
-      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`;
-    const res = await db.query(query, args);
-
-    return Promise.resolve(res.rows);
-  } catch (error) {
-    console.error(error)
     return Promise.resolve(error);
   }
 }
@@ -59,32 +24,7 @@ async function addUser(args) {
   }
 }
 
-async function publishProduct(args) {
-  try {
-    const query = `UPDATE products SET is_published = true WHERE product_id = $1;`;
-    const res = await db.query(query, args);
-    return Promise.resolve(res.rows);
-  } catch (error) {
-    return Promise.resolve(error);
-  }
-}
-
-async function unpublishProduct(args) {
-  try {
-    const query = `UPDATE products SET is_published = false WHERE product_id = $1;`;
-    const res = await db.query(query, args);
-    return Promise.resolve(res.rows);
-  } catch (error) {
-    return Promise.resolve(error);
-  }
-}
-
 module.exports = {
   getUser,
-  getProducts,
-  getOrders,
-  addUser,
-  insertProduct,
-  publishProduct,
-  unpublishProduct,
+  addUser
 }
