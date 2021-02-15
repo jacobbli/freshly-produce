aaf9302c3744f7bc0aacad6eae528da727f77af2
Jacob Lee <jlee93115@163.com>
2021-02-14 22:59:27 -0800

    Change image size

--- freshly-produce/client/src/components/SubscriptionProductDescription.vue
+++ freshly-produce/client/src/components/SubscriptionProductDescription.vue
@@ -99,20 +99,15 @@ h4 {
   margin: .5rem;
 }
 
-img {
-  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-  width:  100px;
-  height: 125px;
-  margin: 2rem 0;
-}
-
 .product-grid-item-content {
   text-align: center;
 }
 
 .product-grid-item-content img {
-  width: 75%;
-  height: 250px;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  width:  100px;
+  height: 125px;
+  margin: 2rem 0;
 }
 
 .product-price {
